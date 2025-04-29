effde5d43ef86ca1dc423599216a19f1ee8d2daf
vicho314 <vicente.carrasco@usach.cl>
2025-04-29 18:41:22 -0400

    Use generic network host for backend; Docker needs this

--- src/http-common.js
+++ src/http-common.js
@@ -1,13 +1,16 @@
 import axios from "axios";
 
+/*
 const payrollBackendServer = import.meta.env.VITE_PAYROLL_BACKEND_SERVER;
 const payrollBackendPort = import.meta.env.VITE_PAYROLL_BACKEND_PORT;
+*/
 
 console.log(payrollBackendServer)
 console.log(payrollBackendPort)
 
 export default axios.create({
-    baseURL: `http://${payrollBackendServer}:${payrollBackendPort}`,
+   // baseURL: `http://${payrollBackendServer}:${payrollBackendPort}`,
+    baseURL: 'http://0.0.0.0:8090'
     headers: {
         'Content-Type': 'application/json'
     }
