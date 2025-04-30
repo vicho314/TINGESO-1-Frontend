9b5834d72513a5c1ce9d96ac8e260089ac95c09f
vicho314 <vicente.carrasco@usach.cl>
2025-04-30 11:37:36 -0400

    Add initial forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import './App.css'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
 import Home from './components/Home';
+import AddEditFeeType from './components/AddEditFeeType';
+import AddEditDiscount from './components/AddEditDiscount';
+import Calendar from './components/Calendar';
+import ClientForm from './components/ClientForm';
+import DiscountList from './components/DiscountList';
+import FeeTypeList from './components/FeeTypeList';
+import TransactionList from './components/TransactionList';
+import MakeTransaction from './components/MakeTransaction';
+import Report from './components/Report';
+//import Sidemenu from './components/Sidemenu';
 /*
 import EmployeeList from './components/EmployeesList';
 import AddEditEmployee from './components/AddEditEmployee';
 import ExtraHoursList from './components/ExtraHoursList';
 import AddEditExtraHours from './components/AddEditExtraHours';
+*/
 import NotFound from './components/NotFound';
+/*
 import PaycheckList from './components/PaycheckList';
 import PaycheckCalculate from './components/PaycheckCalculate';
 import AnualReport from './components/AnualReport';
@@ -19,17 +31,17 @@ function App() {
           <Navbar></Navbar>
             <Routes>
               <Route path="/home" element={<Home/>} />
-              /*<Route path="/employee/list" element={<EmployeeList/>} />
-              <Route path="/employee/add" element={<AddEditEmployee/>} />
-              <Route path="/employee/edit/:id" element={<AddEditEmployee/>} />
-              <Route path="/paycheck/list" element={<PaycheckList/>} />
-              <Route path="/paycheck/calculate" element={<PaycheckCalculate/>} />
-              <Route path="/reports/AnualReport" element={<AnualReport/>} />
-              <Route path="/extraHours/list" element={<ExtraHoursList/>} />
-              <Route path="/extraHours/add" element={<AddEditExtraHours/>} />
-              <Route path="/extraHours/edit/:id" element={<AddEditExtraHours/>} />
-		*/
-              <Route path="*" element={<NotFound/>} />
+              <Route path="/feetype/add" element={<AddEditFeeType/>} />
+              <Route path="/feetype/edit/:id" element={<AddEditFeeType/>} />
+              <Route path="/feetype/list" element={<FeeTypeList/>} />
+	      <Route path="/discount/add" element={<AddEditDiscount/>} />
+              <Route path="/discount/edit/:id" element={<AddEditDiscount/>} />
+	      <Route path="/discount/list" element={<DiscountList/>} />
+	      <Route path="/transaction/list" element={<TransactionList/>} />
+	      <Route path="/client" element={<ClientForm/>} /> 
+	      <Route path="/calendar" element={<Calendar/>} />
+	      <Route path="/report" element={<Report/>} />   
+	      <Route path="*" element={<NotFound/>} />
             </Routes>
           </div>
       </Router>
--- src/http-common.js
+++ src/http-common.js
@@ -3,14 +3,15 @@ import axios from "axios";
 /*
 const payrollBackendServer = import.meta.env.VITE_PAYROLL_BACKEND_SERVER;
 const payrollBackendPort = import.meta.env.VITE_PAYROLL_BACKEND_PORT;
-*/
+
 
 console.log(payrollBackendServer)
 console.log(payrollBackendPort)
+*/
 
 export default axios.create({
    // baseURL: `http://${payrollBackendServer}:${payrollBackendPort}`,
-    baseURL: 'http://0.0.0.0:8090'
+    baseURL: 'http://0.0.0.0:8090',
     headers: {
         'Content-Type': 'application/json'
     }
